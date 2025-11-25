747167271720d6510535b1c5480701eaa7ce74a4
Quanzhi Wu <1298304838@qq.com>
2025-11-25 15:38:48 +0800

    This update includes:
    1.Introduce the openair package (library(openair)).
    
    2. In the server logic, code for generating wind rose diagrams using the windRose() function has been added.
    
    3. Use renderPlot() to display the wind rose plot in the UI.

--- Wind/app.R
+++ Wind/app.R
@@ -1,5 +1,7 @@
 # Load necessary libraries
 library(shiny)
+library(openair)
+
 
 # Define UI
 ui <- fluidPage(
@@ -27,6 +29,13 @@ server <- function(input, output) {
   dataInput <- reactive({
     req(input$file1)  # Ensure the file is uploaded before proceeding
     read.csv(input$file1$datapath)  # Read the uploaded CSV file
+  })
+  
+  # Generate wind rose plot
+  output$windRosePlot <- renderPlot({
+    data <- dataInput()  # Get the uploaded data
+    req("wind_speed" %in% colnames(data), "wind_direction" %in% colnames(data))
+    windRose(data, ws = "wind_speed", wd = "wind_direction", angle = 30)
   })
   
   # Placeholder for logic later
